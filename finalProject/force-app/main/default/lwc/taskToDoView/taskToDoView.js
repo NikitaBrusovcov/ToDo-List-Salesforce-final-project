bfc9cba6171aefdef6e9113e7a5d501dfaec11f4
Zorochce <zorochce@mail.ru>
2020-10-29 23:59:41 +0300

    added pagination

--- finalProject/force-app/main/default/lwc/taskToDoView/taskToDoView.js
+++ finalProject/force-app/main/default/lwc/taskToDoView/taskToDoView.js
@@ -3,7 +3,11 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { refreshApex } from '@salesforce/apex';
 import { deleteRecord } from 'lightning/uiRecordApi';
 import { updateRecord } from 'lightning/uiRecordApi';
-import fetchTaskToDoList from '@salesforce/apex/TaskToDoController.fetchTaskToDoList';
+
+import getTaskToDoList from '@salesforce/apex/TaskToDoController.getTaskToDoList';
+import getNext from '@salesforce/apex/TaskToDoController.getNext';
+import getPrevious from '@salesforce/apex/TaskToDoController.getPrevious';
+import TotalRecords from '@salesforce/apex/TaskToDoController.TotalRecords';
 
 import ID_FIELD from '@salesforce/schema/SubTask__c.Id';
 import DONE__C_FIELD from '@salesforce/schema/SubTask__c.Done__c';
@@ -48,9 +52,12 @@ export default class TaskToDoView extends LightningElement {
     @track tasks;
     error;
 
+    @track v_Offset=0;
+    @track v_TotalRecords;
+    @track page_size = 6;
     wiredTaskResult;
 
-    @wire(fetchTaskToDoList)
+    @wire(getTaskToDoList,{ v_Offset: '$v_Offset', v_pagesize: '$page_size' } )
      wiredTasks(result){
         this.wiredTaskResult = result;
         if (result.data) {
@@ -60,6 +67,58 @@ export default class TaskToDoView extends LightningElement {
             this.error = result.error;
             this.tasks = undefined;
         }
+    }
+
+    connectedCallback() {
+        TotalRecords().then(result=>{
+            this.v_TotalRecords = result;
+        });
+    }
+
+    previousHandler2(){
+        getPrevious({v_Offset: this.v_Offset, v_pagesize: this.page_size}).then(result=>{
+            this.v_Offset = result;
+            if(this.v_Offset === 0){
+                this.template.querySelector('c-pagination').changeView('trueprevious');
+            }else{
+                this.template.querySelector('c-pagination').changeView('falsenext');
+            }
+        });
+    }
+    
+    nextHandler2(){
+        getNext({v_Offset: this.v_Offset, v_pagesize: this.page_size}).then(result=>{
+            this.v_Offset = result;
+           if(this.v_Offset + 10 > this.v_TotalRecords){
+                this.template.querySelector('c-pagination').changeView('truenext');
+            }else{
+                this.template.querySelector('c-pagination').changeView('falseprevious');
+            }
+        });
+    }
+    
+    changeHandler2(event){
+        const det = event.detail;
+        this.page_size = det;
+    }
+    
+    firstpagehandler(){
+        this.v_Offset = 0;
+        this.template.querySelector('c-pagination').changeView('trueprevious');
+        this.template.querySelector('c-pagination').changeView('falsenext');
+    }
+    
+    lastpagehandler(){
+        this.v_Offset = this.v_TotalRecords - (this.v_TotalRecords)%(this.page_size);
+        console.log(this.v_Offset);
+        console.log(this.v_TotalRecords);
+        console.log(this.page_size);
+        if(this.v_Offset === this.v_TotalRecords){
+            this.v_Offset = this.v_TotalRecords-this.page_size;
+        }
+       
+        this.template.querySelector('c-pagination').changeView('falseprevious');
+        this.template.querySelector('c-pagination').changeView('truenext');
     }
 
 
