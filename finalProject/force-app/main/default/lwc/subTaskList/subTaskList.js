4ec149aead1151e11fc5b4f2f5f0664e5aa9f3cb
Zorochce <zorochce@mail.ru>
2020-10-30 19:24:20 +0300

    splitted lwc components and  design enhancements

---
+++ finalProject/force-app/main/default/lwc/subTaskList/subTaskList.js
@@ -0,0 +1,100 @@
+import { LightningElement,track,api } from 'lwc';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { deleteRecord } from 'lightning/uiRecordApi';
+import { updateRecord } from 'lightning/uiRecordApi';
+
+import ID_FIELD from '@salesforce/schema/SubTask__c.Id';
+import DONE__C_FIELD from '@salesforce/schema/SubTask__c.Done__c';
+
+export default class SubTaskList extends LightningElement {
+    @track showAddSubTaskWindow = false;
+    @track showEditSubTaskWindow = false;
+    @track showNegativeButton;
+    @track showPositiveButton = true;
+    @track positiveButtonLabel = 'Close';
+    @api task;
+    error;
+
+    closeSubTaskEditWindow() {
+        this.showEditSubTaskWindow = false;
+        this.dispatchEvent(new CustomEvent('refreshalert'));
+    }
+
+    showSubTaskEditWindow() {
+      this.showEditSubTaskWindow = true;
+    }
+
+    closeSubTaskAddWindow() {
+        this.showAddSubTaskWindow = false;
+        this.dispatchEvent(new CustomEvent('refreshalert'));
+    }
+
+    showSubTaskAddWindow() {
+      this.showAddSubTaskWindow = true;
+    }
+
+    
+    updateSubTaskStatus(event) {
+        let status = event.target.dataset.done;
+        if(status == 'true'){
+            status = false;
+        } else{
+            status = true;
+        }
+       
+        const fields = {};
+        fields[ID_FIELD.fieldApiName] = event.target.dataset.recordid;
+        fields[DONE__C_FIELD.fieldApiName] = status;
+
+        const recordInput = { fields };
+
+        updateRecord(recordInput)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Subtask status updated',
+                        variant: 'success'
+                    })
+                );
+                this.dispatchEvent(new CustomEvent('refreshalert'));
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error updating status of record',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
+    }
+
+    deleteSubTask(event){
+        const recordId = event.target.dataset.recordid;
+        console.log(recordId);
+        deleteRecord(recordId)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'SubTask deleted',
+                        variant: 'success'
+                    })
+                );  
+                this.dispatchEvent(new CustomEvent('refreshAlert'));
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error deleting record',
+                        message: 'Error deleting record ' + error,
+                        variant: 'error'
+                    })
+                );
+            });
+    }
+
+
+}
--- finalProject/force-app/main/default/lwc/taskToDoView/taskToDoView.js
+++ finalProject/force-app/main/default/lwc/taskToDoView/taskToDoView.js
@@ -1,22 +1,13 @@
 import { LightningElement,  wire, track } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 import { refreshApex } from '@salesforce/apex';
-import { deleteRecord } from 'lightning/uiRecordApi';
-import { updateRecord } from 'lightning/uiRecordApi';
 
 import getTaskToDoList from '@salesforce/apex/TaskToDoController.getTaskToDoList';
 import getNext from '@salesforce/apex/TaskToDoController.getNext';
 import getPrevious from '@salesforce/apex/TaskToDoController.getPrevious';
 import TotalRecords from '@salesforce/apex/TaskToDoController.TotalRecords';
 
-import ID_FIELD from '@salesforce/schema/SubTask__c.Id';
-import DONE__C_FIELD from '@salesforce/schema/SubTask__c.Done__c';
-
-
 export default class TaskToDoView extends LightningElement {
-
-    @track showAddSubTaskWindow = false;
-    @track showEditSubTaskWindow = false;
     @track showEditTaskWindow = false;
     @track showNegativeButton;
     @track showPositiveButton = true;
@@ -29,24 +20,6 @@ export default class TaskToDoView extends LightningElement {
 
     showTaskEditWindow() {
       this.showEditTaskWindow = true;
-    }
-
-    closeSubTaskEditWindow() {
-        this.showEditSubTaskWindow = false;
-        return refreshApex(this.wiredTaskResult);
-    }
-
-    showSubTaskEditWindow() {
-      this.showEditSubTaskWindow = true;
-    }
-
-    closeSubTaskAddWindow() {
-        this.showAddSubTaskWindow = false;
-        return refreshApex(this.wiredTaskResult);
-    }
-
-    showSubTaskAddWindow() {
-      this.showAddSubTaskWindow = true;
     }
 
     @track tasks;
@@ -122,66 +95,9 @@ export default class TaskToDoView extends LightningElement {
     }
 
 
-    updateSubTaskStatus(event) {
-        let status = event.target.dataset.done;
-        if(status == 'true'){
-            status = false;
-        } else{
-            status = true;
-        }
-       
-        const fields = {};
-        fields[ID_FIELD.fieldApiName] = event.target.dataset.recordid;
-        fields[DONE__C_FIELD.fieldApiName] = status;
-
-        const recordInput = { fields };
-
-        updateRecord(recordInput)
-            .then(() => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Subtask status updated',
-                        variant: 'success'
-                    })
-                );
-                return refreshApex(this.wiredTaskResult);
-            })
-            .catch(error => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error updating status of record',
-                        message: error.body.message,
-                        variant: 'error'
-                    })
-                );
-            });
+    refreshApexCode(){
+        console.log('alert');
+        return refreshApex(this.wiredTaskResult);
     }
-
-    deleteSubTask(event){
-        const recordId = event.target.dataset.recordid;
-        console.log(recordId);
-        deleteRecord(recordId)
-            .then(() => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'SubTask deleted',
-                        variant: 'success'
-                    })
-                );
-                return refreshApex(this.wiredTaskResult);
-            })
-            .catch((error) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error deleting record',
-                        message: 'Error deleting record ' + error,
-                        variant: 'error'
-                    })
-                );
-            });
-    }
-
 }
 
